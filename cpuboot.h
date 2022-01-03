b8cef5067d8a2995d2f8562f2cbc3667f530bf75
li Insects Awaken <zhex.h.li@intel.com>
2022-01-03 19:32:49 +0800

    the name of kthread

--- cpuboot.h
+++ cpuboot.h
@@ -2,6 +2,7 @@
  * cpu 可插拔线程描述符
  */
 struct cpu_hotplug_kthread {
+    const char *kthread_name;
     void (*gener_kthread) (unsigned int cpu); // 为每个onine cpu生成对应的 kthread.
  
     struct list_head  list;                   // 链接到全局链表上.
