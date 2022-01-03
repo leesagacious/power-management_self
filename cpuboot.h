6ebd4d5990d0cb026b788a6f8a11c3ba9ebe5f72
li Insects Awaken <zhex.h.li@intel.com>
2022-01-03 21:22:02 +0800

    add cpu hotplug state

--- cpuboot.h
+++ cpuboot.h
@@ -7,3 +7,11 @@ struct cpu_hotplug_kthread {
  
     struct list_head  list;                   // 链接到全局链表上.
 };
+
+struct cpu_hotplug_state {
+   /*
+    * 这里不使用枚举来表示cpu 热插拔的状态.
+    */
+    unsigned int cur_state;                 // 当前cpu 热插拔状态
+    unsigned int tar_state;                 // 目标cpu 热插拔状态
+};
