d43e8a1e0cebed7198d4c8503753aadc6308559b
li Insects Awaken <zhex.h.li@intel.com>
2022-01-05 20:26:38 +0800

    create kthread on assign cpu

--- cpuboot.c
+++ cpuboot.c
@@ -9,11 +9,18 @@ static LIST_HEAD(cpu_hotplug_order);  // cpu热插拔步骤链表
 int cpuboot_generate_percpu_kthread(struct cpu_hotplug_kthread *hp_kthread)
 {
     unsigned int cpu;
+    struct task_struct *cpu_kthread;
   
     mutex_lock(&cpuboot_hp_kthread_lock);
     
     for_each_online_cpu(cpu) {        // 为每个online cpu 创建热插拔内核线程
-        
+        /*
+         * 在 unsigned int cpu 上创建内核线程.
+         */
+        cpu_kthread = kthread_create_on_cpu(cpu_hp_thread_fn, NULL,
+                        cpu, hp_kthread->kthread_name);
+        if (IS_ERR(cpu_kthread))
+            return PTR_ERR(cpu_kthread);
     }
   
     list_add(&hp_kthread->list, &hp_kthread_list);  // 添加到全局链表 hp_kthread_list 上
