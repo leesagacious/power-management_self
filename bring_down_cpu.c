7ea38186af294b2a821225254d9098a51eff8851
li Insects Awaken <zhex.h.li@intel.com>
2022-01-18 00:22:27 +0800

    add bring_cpu

---
+++ bring_down_cpu.c
@@ -0,0 +1,20 @@
+/*
+ *    #func . 把 cpu id 为 cpu的状态 唤醒到热插拔 target_state 状态.
+ *    @cpu . 要操作的cpu id
+ *    @target_state . 把cpu 唤醒到 热插拔的状态
+ */
+static int bring_cpu(unsigned int cpu, unsigned int 
+            target_state)
+{
+    int ret = 0;
+    
+   /*
+    *  如果 cpu的hp状态已经大于或者等于了所要唤醒的状态
+    *  那么执行该函数后续的唤醒状态操作是无用的.
+    */
+    if ()
+      
+    
+  
+    return ret;
+}
